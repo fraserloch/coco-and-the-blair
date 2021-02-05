3f06346576f8bf9552e1e9fcc724137aa3338aad
frlo <loch@cyviz.com>
2021-02-05 11:06:20 +0000

    finishing sign in handle submti

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,12 +16,18 @@ class SignIn extends Component {
       }
    }
 
-   handleSubmit = event => {
+   handleSubmit = async event => {
       event.preventDefault();
-      this.setState({
-         email: '',
-         password: ''
-      })
+
+      const { password, email } = this.state;
+
+      try {
+         await auth.signInWithEmailAndPassword(email, password);
+         this.setState({ email: '', password: '' });
+      } catch (error) {
+         console.log(error);
+      }
+
    }
 
    handleChange = event => {
@@ -55,7 +61,7 @@ class SignIn extends Component {
                   required
                />
 
-               <div className="buttons"> 
+               <div className="buttons">
                   <CustomButton type="submit" value="Submit Form"> Sign in</CustomButton>
                   <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google</CustomButton>
                </div>
