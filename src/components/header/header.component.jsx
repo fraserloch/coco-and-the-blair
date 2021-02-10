e578b14d90126430a426dfaf463424408874df91
frlo <loch@cyviz.com>
2021-02-10 10:09:12 +0000

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import { auth } from '../../firebase/firebase.utils';
+import { createStructuredSelector } from 'reselect';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selector';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -37,12 +39,9 @@ const Header = ({ currentUser, hidden }) => (
    </div>
 )
 
-const mapStateToProps = ({
-   user: { currentUser },
-   cart: { hidden }
-}) => ({
-   currentUser,
-   hidden
+const mapStateToProps = createStructuredSelector({
+   currentUser: selectCurrentUser,
+   hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,7 +7,18 @@ export const selectCartItems = createSelector(
    (cart) => cart.cartItems
 )
 
+export const selectCartHidden = createSelector(
+   [selectCart],
+   (cart) => cart.hidden
+)
+
 export const selectCartItemsCount = createSelector(
    [selectCartItems],
    (items) => items.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+)
+
+export const selectCartTotal = createSelector(
+   [selectCartItems],
+   (items) => items.reduce((acc, cartItem) => acc + cartItem.quantity * cartItem.price
+   , 0)
 )
